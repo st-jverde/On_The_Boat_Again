048d482c62666f03151f8a421ad4dfa0c9b68910
Joeri Verdegaal <joeriverdegaal@gmail.com>
2022-08-24 14:57:27 +0200

    added show booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @boat = Boat.find(params[:boat_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @boat = Boat.find(params[:boat_id])
+    @booking.user = current_user
+    @booking.boat = @boat
+
+    if @booking.save
+      redirect_to profile_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :cost, :boat_id, :user_id)
+  end
 end
