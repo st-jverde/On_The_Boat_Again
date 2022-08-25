42a4391ab78051a73b9609e67c13cb9628c2a0dd
Anja Ottiger <anjakyraottiger@gmail.com>
2022-08-25 13:07:36 +0200

    profile page changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,15 @@ class PagesController < ApplicationController
   def profile
     @my_boats = current_user.boats
     @bookings = current_user.bookings
+    # @rented_boats = current_user.bookings.boat.name
+    @my_rentals = []
+    @bookings.each do |booking|
+      @my_rentals << booking.boat
+    end
+
+    # rented_boats_names = []
+    # @boats.each do |boat|
+    #   rented_boats_names << boats.name
+    # end
   end
 end
