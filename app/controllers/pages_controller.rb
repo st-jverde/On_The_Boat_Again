cfd6f41487b22fd9de0a458ee480cb5789cb284e
Anja Ottiger <anjakyraottiger@gmail.com>
2022-08-25 15:12:19 +0200

    log out fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
   end
 
   def profile
-    @my_boats = current_user.boats
-    @bookings = current_user.bookings
-    # @rented_boats = current_user.bookings.boat.name
-    @my_rentals = []
-    @bookings.each do |booking|
-      @my_rentals << booking.boat
-    end
-
-    # rented_boats_names = []
-    # @boats.each do |boat|
-    #   rented_boats_names << boats.name
-    # end
   end
 end
